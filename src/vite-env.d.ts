6600efc79a304a3921a2f3225653570d69ec65a6
ayan-ali-khan <162182794+ayan-ali-khan@users.noreply.github.com>
2025-01-13 22:14:23 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_NHOST_SUBDOMAIN=fevxqwbyflusyocslrzs
-  VITE_NHOST_REGION=ap-south-1
-  VITE_RAPID_API_KEY=79a448c6afmsh85d8bfb3a9ddf7ep1cc724jsnbd212e34337b
+  VITE_NHOST_SUBDOMAIN=
+  VITE_NHOST_REGION=
+  VITE_RAPID_API_KEY=
 }
 
 interface ImportMeta {
